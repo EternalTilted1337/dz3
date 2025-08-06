521f2535affbe80e8d1cde33510e5c4f9abc7526
Mihail <anitikrev@gmail.com>
2025-08-07 02:59:29 +0300

    check file

---
+++ rufffff.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import os  # повторный импорт
+import  math
+
+def test( ):
+  print(  "Hello" )
+
+def test( ):
+  print("Duplicate function")
+
+#код до
+# import os
+# import sys
+# import os  # повторный импорт
+# import  math
+#
+# def test( ):
+#   print(  "Hello" )
+#
+# def test( ):
+#   print("Duplicate function")
